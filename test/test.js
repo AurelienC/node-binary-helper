05c7f64f3fd80e0185f6b6a274b4fbf615c5f784
Aurélien <framasoft@courriel.aureliendistant.fr>
2017-05-03 15:49:38 +0200

    Add functions

--- test/test.js
+++ test/test.js
@@ -13,9 +13,38 @@ describe('binary helper', () => {
     expect(r).to.eql(34);
   });
 
-  it('addLsbMsb', () => {
-    var r = bh.addLsbMsb(52501, 1883);
-    expect(r).to.eql(123456789);
+  it('setBCD', () => {
+    var r = bh.setBCD(12, 34);
+    expect(r).to.eql(1234);
+  });
+
+  it('setMsbLsb', () => {
+    var r = bh.setMsbLsb(0x25, 0x83);
+    expect(r).to.eql(0x2583);
+  });
+
+  it('setMsbLsbDouble', () => {
+    var r = bh.setMsbLsbDouble(0x2546, 0x8317);
+    expect(r).to.eql(0x25468317);
+  });
+
+  it('bitsToWord', () => {
+    var r = bh.bitsToWord([ 1, 1, 0, 1]);
+    expect(r).to.eql(11);
+  });
+
+  it('toSigned', () => {
+    var r = bh.toSigned(255, 8);
+    expect(r).to.eql(-128);
+
+    var r = bh.toSigned(127, 8);
+    expect(r).to.eql(127);
+
+    var r = bh.toSigned(128, 8);
+    expect(r).to.eql(-1);
+
+    var r = bh.toSigned(0, 8);
+    expect(r).to.eql(0);
   });
 
   it('charArrayToString', () => {
